da4ac989938df713d12113f975a16f52836d0619
Simona <simona.matiukaite@gmail.com>
2017-07-28 22:56:49 +0300

    no message

---
+++ header.php
@@ -0,0 +1,49 @@
+<!-- Menu icons -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid menu">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Lietuvos Raudonoji knyga</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li title="Pradinis puslapis"><a href="index.php" target="">
+        <img src="icons/home.png" alt="namai" width="40" height="40" />
+        <div class="namai">
+        </div>
+      </a></li>
+
+      <li title="Apie..."><a href="#Apie" target="">
+        <img src="icons/apie.png" alt="apie" width="38" height="38" />
+      </a></li>
+
+      <li title="Galerija"><a href="#Galerija" target="">
+          <img src="icons/gallery.png" alt="galerija" width="40" height="40"/>
+        </a></li>
+
+        <li title="Susiję įstatai"><a href="#istatai" target="">
+            <img src="icons/law.png" alt="istatai" width="40" height="40"/>
+          </a></li>
+
+      <li title="Susisiekime"><a href="#Susisiekime" target="">
+          <img src="icons/mail.png" alt="pastas" width="40" height="40" />
+        </a></li>
+    </ul>
+
+    <form action="" method="post" class="navbar-form navbar-left">
+      <div class="form-group">
+        <input type="text" name="name" class="form-control" placeholder="Paieška" />
+      </div>
+      <button type="submit" class="btn btn-default">Tvirtinti</button>
+    </form>
+  </div>
+</nav>
+<!-- <script type="text/javascript">
+          $(function() {
+
+            //autocomplete
+            $(".auto").autocomplete({
+              source: "search.php",
+              minLength: 1
+            });
+
+          });
+        </script> -->
